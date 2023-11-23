a86486481a0b2faa7e5aac92a3afeeb42db39077
Zhylinska <irynazhylinska83@gmail.com>
2023-11-23 12:48:27 +0100

    kitchen react and oline food shop moved to separate repos

--- portfolio/react_kitchen/src/dataExamples.js
+++
@@ -1,9 +0,0 @@
-const dataExamples=[
-    {id: 1,
-        img: "/img/slide5.jpg"},
-    {id: 2,
-        img: "/img/slide6.jpg"},
-    {id: 3,
-        img: "/img/slide7.jpg"},
-]
-export default dataExamples
