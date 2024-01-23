1ef1d59e764424dd0d881b99d311a2be2f187486
Zhylinska <irynazhylinska83@gmail.com>
2024-01-23 20:48:18 +0100

    remove slick, jquery

--- my_portfolio.js
+++ my_portfolio.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('DOMContentLoaded', () => {
     const langSwitcher = (e) => document.querySelector('body').className = e.target.dataset.langClass;
     const enableLangSwitcher = (elementId) => document
@@ -7,8 +6,53 @@ window.addEventListener('DOMContentLoaded', () => {
 
     enableLangSwitcher('enLangSwitcher');
     enableLangSwitcher('deLangSwitcher');
-    $(".slider-container").slick();
 
-    $(".slick-prev").text("❮");
-    $(".slick-next").text("❯");
+
 });
+
+
+const slider = document.querySelector(".slider-container");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const slides = Array.from(slider.querySelectorAll(".slide"));
+let slideIndex = 0;
+
+prev.addEventListener('click', showPrev);
+next.addEventListener('click', showNext);
+
+function showPrev() {
+
+    if (slideIndex === 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = slideIndex - 1;
+    }
+    updateSlider();
+}
+
+function showNext() {
+
+    if (slideIndex === slides.length - 1) {
+        slideIndex = 0;
+    } else {
+        slideIndex = slideIndex + 1;
+    }
+    updateSlider();
+}
+
+
+function updateSlider() {
+
+    slides.map((slide, currentIndex) => {
+        if (currentIndex === slideIndex) {
+
+            slide.classList.add("active")
+            console.log("active")
+        } else {
+            slide.classList.remove("active")
+        }
+    })
+}
+
+
+
